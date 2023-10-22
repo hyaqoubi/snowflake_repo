63ca0089f56d0263f79b4a1aaf47a00d2c20168d
hyaqoubi <148675534+hyaqoubi@users.noreply.github.com>
2023-10-22 10:52:43 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,4 +5,4 @@ streamlit.header('🥣 Breakfast Menu')
 streamlit.text('🥗 Omega 3 & Blueberry Oatmeal')
 streamlit.text('🐔 Kale, Spinach & Rocket Smoothie')
 streamlit.text('🍞🥑 Hard-Boiled Free-Range Egg')
-    
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
