128c9f9f1e04a7f39c1366cabee4a18451ef5601
hyaqoubi <148675534+hyaqoubi@users.noreply.github.com>
2023-10-22 10:52:09 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,8 @@
 import streamlit
 
 streamlit.title("Youhoo my first Streamlit app is here!!")
+streamlit.header('🥣 Breakfast Menu')
+streamlit.text('🥗 Omega 3 & Blueberry Oatmeal')
+streamlit.text('🐔 Kale, Spinach & Rocket Smoothie')
+streamlit.text('🍞🥑 Hard-Boiled Free-Range Egg')
+    
